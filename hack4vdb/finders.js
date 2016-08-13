4d7db986dea53a86cea11368c5a2f6f482030851
Tom Peak <tom@evocell.net>
2016-08-13 23:49:32 +0200

    scss hated

---
+++ hack4vdb/finders.js
@@ -0,0 +1,6 @@
+from django.core.files.storage import FileSystemStorage
+from django.contrib.staticfiles.finders import BaseStorageFinder
+from django.conf import settings
+
+class StaticFinder(BaseStorageFinder):
+    storage = FileSystemStorage(settings.STATIC_ROOT, settings.STATIC_URL)
