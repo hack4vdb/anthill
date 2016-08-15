baad003ad281bb09fba8e6750b478f95a63c71ee
stefan <dev@2904.cc>
2016-08-16 00:26:26 +0200

    fixed typo in meetupsbylanglng

--- hack4vdb/urls.py
+++ hack4vdb/urls.py
@@ -42,7 +42,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include(apiRouter.urls)),
     url(r'^api/meetups/(?P<meetupid>[\d\w-]+)/partake/(?P<userid>[\d\w-]+)/', views.partake_meetup, name='partake_meetup'),
-    url(r'^api/meetupsbylanglng/(?P<latlong>.+)/', views.meetupsByLatLng, name='meetupsbylanglng'),
+    url(r'^api/meetupsbylatlng/(?P<latlong>.+)/', views.meetupsByLatLng, name='meetupsbylatlng'),
     url(r'^api/interestingplaces/(?P<id>[\d\w-]+)/', views.interesting_places, name='interesting_places'),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
