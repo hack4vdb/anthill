11ad8ecec89ef2535d85047292c644e3a9fac771
Ben Freundorfer <benjamin@freundorfer.at>
2016-08-19 00:03:22 +0200

    Fix default coordinates if no postalcode found

--- anthill/models.py
+++ anthill/models.py
@@ -275,24 +275,18 @@ class PostalcodeCoordinates(models.Model):
     @staticmethod
     def get_coordinates(postalcode):
         postalcoords = PostalcodeCoordinates.objects.filter(postalcode=postalcode).first()
-        if not postalcode:
-            return GEOSGeometry('POINT(%f %f)'.format(14.3, 47.5), srid=4326)
+        if not postalcoords:
+            return GEOSGeometry('POINT({} {})'.format(14.3, 47.5), srid=4326)
         return postalcoords.coordinate
 
-    def __str__(self):
-        return self.__unicode__()
-
     def __unicode__(self):
-        return str(self.postalcode)
+        return unicode(self.postalcode)
 
 class InterestingPlaces(models.Model):
     title = models.CharField(max_length=1000)
     postalcode = models.IntegerField()  # PLZ (4-digit)
     city = models.CharField(max_length=500)  # Ort
     coordinate = models.PointField()
-
-    def __str__(self):
-        return self.__unicode__()
 
     def __unicode__(self):
         return '{} {}: {}'.format(self.postalcode, self.city, self.title)
