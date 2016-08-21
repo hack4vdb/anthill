959a093ee8ce5726ea35abfaec0e2a3d33982553
Martin Schürrer <martin@schuerrer.org>
2016-08-22 00:39:19 +0200

    Make sure none of the responses get cached

--- hack4vdb/settings.py
+++ hack4vdb/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'mobileesp.middleware.MobileDetectionMiddleware',
+    'hack4vdb.middleware.CacheControl',
 ]
 
 CRONJOBS = [
