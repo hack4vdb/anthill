a479669803c058f777ffcf7a989d13978b96b2fb
c3o <github@c3o.org>
2016-08-21 16:06:04 +0200

    Invite: Add WhatsApp, FB Messenger & Telegram if on mobile

--- hack4vdb/settings.py
+++ hack4vdb/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'mobileesp.middleware.MobileDetectionMiddleware',
 ]
 
 ROOT_URLCONF = 'hack4vdb.urls'
