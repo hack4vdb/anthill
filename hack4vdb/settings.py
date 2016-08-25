339334390f825b3b684620e458ee825f5f4975ad
Lukas Martini <hello@lutoma.org>
2016-08-25 21:54:01 +0200

    Drop cache control middle ware (now done server-side)

--- hack4vdb/settings.py
+++ hack4vdb/settings.py
@@ -64,7 +64,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'mobileesp.middleware.MobileDetectionMiddleware',
-    'hack4vdb.middleware.CacheControl',
 ]
 
 CRONJOBS = [
